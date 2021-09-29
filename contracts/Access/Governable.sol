179e05e9f4d9c64511332c92beaa30ecb6c1a7e7
Mohsin Laeeque <mohsin.laeeque@arhamsoft.com>
2021-09-29 12:25:30 +0500

    Gov file

--- contracts/Access/Governable.sol
+++ contracts/Access/Governable.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
 
 contract Configurable is OwnableUpgradeSafe {
 
